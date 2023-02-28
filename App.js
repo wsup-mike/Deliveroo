b6ccd07d05fb2087d254cae541465f34de335440
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-28 12:02:23 -0600

    import createNativeStackNav - cr8 'Stack'

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
