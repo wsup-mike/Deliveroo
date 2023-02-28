a3682084948b1a4e347875e175a07988f845c6b6
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-28 15:30:16 -0600

    HomeScreen-Add useLayoutEffect

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,19 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
     <View>
-      <Text className="text-red-400">This is my Home Screen</Text>
+      <Text className="text-red-400 ">This is my Home Screen</Text>
     </View>
   );
 };
