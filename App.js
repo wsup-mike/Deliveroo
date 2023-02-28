a4840768f291586705da455dd2dca14be54cfa4e
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-28 11:17:49 -0600

    import NavigationContainer

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
