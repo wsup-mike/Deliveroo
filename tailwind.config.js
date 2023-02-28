ff28f2b805102bec9d0fcb8ff229f38898e333af
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-28 11:01:31 -0600

    change content in tailwind.config .js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
