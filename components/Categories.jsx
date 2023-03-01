f1a62813a1901c1c29a43b374f0214229112758e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 07:16:44 -0600

    Edit CategoryCard - no scrollview or atts

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 
 const Categories = () => {
   return (
-    <View>
-      <Text>Categories (Bitches!)</Text>
-    </View>
+    <ScrollView
+      horizontal 
+      showsHorizontalScrollIndicator={false} 
+      contentContainerStyle={{
+          paddingHorizontal: 15,
+          paddingTop: 10,
+      }}
+    >
+      {/* Category Cards here */}
+    <Text>Categories (Bitches!)</Text>
+    </ScrollView>
   )
 }
 
--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -1,18 +1,11 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
 const CategoryCard = () => {
   return (
-    <ScrollView 
-        horizontal 
-        showsHorizontalScrollIndicator={false} 
-        contentContainerStyle={{
-            paddingHorizontal: 15,
-            paddingTop: 10,
-        }}
-    >
-      <Text>CategoryCard</Text>
-    </ScrollView>
+    <View>
+      <Text>Single Category Card</Text>
+    </View>
   )
 }
 
