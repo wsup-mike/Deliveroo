735471954e2b2fdb5719203353390d1e6fa3f2bf
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-28 18:11:57 -0600

    Add 2 lines Text

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,11 +15,15 @@ const HomeScreen = () => {
     <SafeAreaView>
       <Text className="text-red-400 ">
         {/*Header*/}
-        <View>
+        <View className="flex-row">
           <Image
             source={{ uri: "https://links.papareact.com/wru" }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
+          <View>
+            <Text>Deliver Now!</Text>
+            <Text>Current Location</Text>
+          </View>
         </View>
       </Text>
     </SafeAreaView>
