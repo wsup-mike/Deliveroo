aa745e7ace4a9a54017e36df4ba3fcbc06fa04e8
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-28 18:06:02 -0600

    Add driver icon image in Header on HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 
@@ -13,7 +13,15 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text className="text-red-400 ">This is my Home Screen</Text>
+      <Text className="text-red-400 ">
+        {/*Header*/}
+        <View>
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+        </View>
+      </Text>
     </SafeAreaView>
   );
 };
