3fd133ff2709c6b5d07ccf3076e69d5600144443
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-28 20:01:47 -0600

    New ScrollView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import {
@@ -50,6 +57,9 @@ const HomeScreen = () => {
           </View>
           <AdjustmentsVerticalIcon color={"#00ccbb"} />
         </View>
+
+        {/* Body */}
+        <ScrollView></ScrollView>
       </View>
     </SafeAreaView>
   );
