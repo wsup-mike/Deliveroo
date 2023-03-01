739f0e7f22c57141bcf0fbce352ade1073650780
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 09:05:07 -0600

    RestaurantCard.jsx-Set up props

--- components/RestaurantCard.jsx
+++ components/RestaurantCard.jsx
@@ -1,7 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const RestaurantCard = () => {
+const RestaurantCard = ({
+    id,
+    imgUrl,
+    title,
+    rating,
+    genre,
+    address,
+    short_description,
+    dishes,
+    long,
+    lat,
+}) => {
   return (
     <View>
       <Text>RestaurantCard</Text>
