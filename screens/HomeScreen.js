bb8bad9568a773679a564c5694f6a9657ee4265a
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 07:08:11 -0600

    import and create ScrollView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -27,47 +27,43 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <View>
-        {/*Header*/}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
-          <Image
-            source={{ uri: "https://links.papareact.com/wru" }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color={"#00ccbb"} />
-            </Text>
-          </View>
-
-          <UserIcon size={35} color={"#00ccbb"} />
+      {/*Header*/}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color={"#00ccbb"} />
+          </Text>
         </View>
 
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-            <MagnifyingGlassIcon color="gray" size={20} />
-            <TextInput
-              placeholder="Restaurants and cuisines"
-              keyboardType="default"
-            />
-          </View>
-          <AdjustmentsVerticalIcon color={"#00ccbb"} />
-        </View>
-
-        {/* Body */}
-        <ScrollView className="bg-gray-100">
-          {/* Categories */}
-          <Categories />
-          {/* Offers */}
-
-          {/* Featured */}
-        </ScrollView>
+        <UserIcon size={35} color={"#00ccbb"} />
       </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color={"#00ccbb"} />
+      </View>
+
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+        {/* Offers */}
+
+        {/* Featured */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
