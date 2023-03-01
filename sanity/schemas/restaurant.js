47a121e4401959406afa9ea5c6ab4feb075d0b21
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 14:08:36 -0600

    Create fields for restaurant.js

--- sanity/schemas/restaurant.js
+++ sanity/schemas/restaurant.js
@@ -2,64 +2,66 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'restaurant',
-  title: 'Post',
+  title: 'Restaurant',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    }),
+    {
+      name: "name",
+      type: "string",
+      title: "Restaurant name",
+      validation:(Rule) => Rule.required(),
+    },
+    {
+      name: "short_description",
+      type: "string",
+      title: "Short description",
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image of the Restaurant",
+    },
+    {
+      name: "lat",
+      type: "number",
+      title: "Latitude of the Restaurant",
+    },
+    {
+      name: "long",
+      type: "number",
+      title: "Longitude of the Restaurant",
+    },
+    {
+      name: "address",
+      type: "string",
+      title: "Restaurant address",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "rating",
+      type: "number",
+      title: "Enter a Rating from (1-5 stars)",
+      validation: (Rule) => Rule.required()
+        .min(1)
+        .max(5)
+        .error("Please enter a value between 1 and 5")
+    },
+    {
+      name: "type",
+      title: "Category",
+      validation: (Rule) => Rule.required(),
+      type: "reference",
+      to: [{ type: "category "}],
+    },
+    {
+      name: "dishes",
+      type: "array",
+      title: "Dishes",
+      of: [{ 
+        type: "reference",
+        to: [{ type: "dish"}],
+      }]
+    },
   ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
-    },
-  },
-})
+}
