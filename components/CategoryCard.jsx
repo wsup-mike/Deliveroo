1029e42efbec188b1d3b90f5093ce1e343217ede
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 07:34:48 -0600

    CategoryCard-apply atts to Image

--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -2,10 +2,13 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 
-const CategoryCard = ({ imgUri, title }) => {
+const CategoryCard = ({ imgUrl, title }) => {
   return (
     <TouchableOpacity>
-        <Image source={{ uri: imgUri }} />
+        <Image 
+            source={{ uri: imgUrl }} 
+            className="h-20 w-20 rounded"
+        />
         <Text>{title}</Text>
     </TouchableOpacity>
   )
