c05269f87b47a5daec3aae7c15bc08baaab36219
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 08:49:56 -0600

    FeaturedRow-Add text n ScrollView

--- components/FeaturedRow.jsx
+++ components/FeaturedRow.jsx
@@ -1,15 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
+
 
 const FeaturedRow = ({ id, title, featuredCategory, description  }) => {
   return (
     <View>
-      <View>
+      <View className="mt-4 flex-row items-center justify-between px-4">
         <Text className="font-bold text-lg" >{title}</Text>
-        <ArrowRightIcon />
+        <ArrowRightIcon color="#00ccbb" />
       </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+      <ScrollView>
+
+      </ScrollView>
     </View>
+    
   )
 }
 
