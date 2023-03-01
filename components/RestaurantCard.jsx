bdfa221bb1e85c419bab2861cf83bef4ebf9d6af
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 10:05:52 -0600

    Import 2 icons here and incorporate

--- components/RestaurantCard.jsx
+++ components/RestaurantCard.jsx
@@ -1,5 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react';
+import { StarIcon } from 'react-native-heroicons/solid';
+
 
 const RestaurantCard = ({
     id,
@@ -23,8 +25,11 @@ const RestaurantCard = ({
         />
         <View className="px-3 pb-4">
             <Text className="font-bold text-lg pt-2">{title}</Text>
-            <View>
-                
+            <View className="flex-row items-center space-x-1">
+                <StarIcon color='green' opacity={0.5} size={22} />
+                <Text className="text-xs text-gray-500">
+                    <Text className="text-green-700">{rating}</Text> · {genre}
+                </Text>
             </View>
         </View>
     </TouchableOpacity>
