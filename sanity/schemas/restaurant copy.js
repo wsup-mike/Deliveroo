30337fffcecceb376edba419c1c53ab8fd76a5d6
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 15:56:20 -0600

    Refactor restaurant.js 2 include defineField()

--- sanity/schemas/restaurant copy.js
+++ sanity/schemas/restaurant copy.js
@@ -5,54 +5,54 @@ export default defineType({
   title: 'Restaurant',
   type: 'document',
   fields: [
-    defineField({
+    {
       name: 'name',
       type: 'string',
       title: 'Restaurant name',
       validation: (Rule) => Rule.required(),
-    }),
-    defineField({
+    },
+    {
       name: 'short_description',
       type: 'string',
       title: 'Short description',
       validation: (Rule) => Rule.max(200),
-    }),
-    defineField({
+    },
+    {
       name: 'image',
       type: 'image',
       title: 'Image of the Restaurant',
-    }),
-    defineField({
+    },
+    {
       name: 'lat',
       type: 'number',
       title: 'Latitude of the Restaurant',
-    }),
-    defineField({
+    },
+    {
       name: 'long',
       type: 'number',
       title: 'Longitude of the Restaurant',
-    }),
-    defineField({
+    },
+    {
       name: 'address',
       type: 'string',
       title: 'Restaurant address',
       validation: (Rule) => Rule.required(),
-    }),
-    defineField({
+    },
+    {
       name: 'rating',
       type: 'number',
       title: 'Enter a Rating from (1-5 stars)',
       validation: (Rule) =>
         Rule.required().min(1).max(5).error('Please enter a value between 1 and 5'),
-    }),
-    defineField({
+    },
+    {
       name: 'type',
       title: 'Category',
       validation: (Rule) => Rule.required(),
       type: 'reference',
       to: [{type: 'category '}],
-    }),
-    defineField({
+    },
+    {
       name: 'dishes',
       type: 'array',
       title: 'Dishes',
@@ -62,6 +62,6 @@ export default defineType({
           to: [{type: 'dish'}],
         },
       ],
-    }),
+    },
   ],
 })
