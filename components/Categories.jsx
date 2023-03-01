0005a27c471a111057a682b40a16d76f182fcaa0
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-01 07:59:00 -0600

    Finish CategoryCard changes

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -13,12 +13,12 @@ const Categories = () => {
       }}
     >
       {/* Category Cards here */}
-    <Text>Categories (Bitches!)</Text>
-    <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing1" />
-    <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing2" />
-    <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing3" />
-    <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing4" />
-    <CategoryCard imgUrl='https://links.papareact.com/wru' title="Testing5" />
+    {/* <Text>Categories (Bitches!)</Text> */}
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="Testing1" />
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="Testing2" />
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="Testing3" />
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="Testing4" />
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="Testing5" />
     </ScrollView>
   )
 }
--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 const CategoryCard = ({ imgUrl, title }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className="mr-2 relative">
         <Image 
             source={{ uri: imgUrl }} 
             className="h-20 w-20 rounded"
         />
-        <Text>{title}</Text>
+        <Text className="absolute bottom-1 left-1 text-white font-bold">{title}</Text>
     </TouchableOpacity>
   )
 }
