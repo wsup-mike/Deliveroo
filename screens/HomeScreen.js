912990823e12f9d7c2fdc7c97ac93051be0c2cdd
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-02 08:36:09 -0600

    HomeScreen-import and cr8 new useEffect featuredCa

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,7 @@ import {
   TextInput,
   ScrollView,
 } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/core";
 import {
   UserIcon,
@@ -19,6 +19,7 @@ import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
