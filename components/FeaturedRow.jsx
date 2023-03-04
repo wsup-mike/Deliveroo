ff1f61e271fc9fa85942de5372bd0fc6a062a8cd
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-04 17:20:47 -0600

    Change useEffect

--- components/FeaturedRow.jsx
+++ components/FeaturedRow.jsx
@@ -7,13 +7,31 @@ import "react-native-url-polyfill/auto";
 import querystring from "querystring";
 
 
+//4 featured category ids
+// 07be6f5c-e806-4b93-b1b1-b35bf7ebaaf4
+// 1816b133-f591-4169-b0d4-1c13b6cd8479
+// 474cd9ae-ad57-41ce-a8c8-404e78a3963b
+// a0cb9eb9-22c2-4df3-a8bc-0c2f1e79de2a
+
 const FeaturedRow = ({ id, title, featuredCategory, description  }) => {
   const [featuredRow, setFeaturedRow] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try { 
-        const data = await client.fetch(`*[_type == "restaurant"]`);
+        const data = await client.fetch(`
+          *[_type == "featured" && _id == $id] {
+            ...,
+            restaurants[]->{
+              ...,
+              dishes[]->,
+              type->{
+                name
+              }
+            },
+          } [0]
+        `, { id }
+        );
 
         const parsedData = JSON.parse(
           JSON.stringify(data).replace(/\bURLSearchParams\b/g, "querystring")
