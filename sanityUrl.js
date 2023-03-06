325b294fea870e17d06aa34a79939bfc3b1a1a08
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-06 10:03:58 -0600

    export urlFor

--- sanityUrl.js
+++ sanityUrl.js
@@ -4,6 +4,6 @@ import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(client);
 
-function urlFor(source) {
+export function urlFor(source) {
   return builder.image(source);
 }
