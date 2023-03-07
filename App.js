211983391ae2ce603039b0c49782583d02904d5c
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-07 10:36:35 -0600

    App.js-import RestaurantScreen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
 
 const Stack = createNativeStackNavigator();
 
