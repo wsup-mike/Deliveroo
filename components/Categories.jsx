89c869f91fc0570683fb37be0b3a2efd4948d814
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-07 09:28:54 -0600

    Categories-import useEffect

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,8 +1,11 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CategoryCard from './CategoryCard';
 
+
 const Categories = () => {
+
+ 
   return (
     <ScrollView
       horizontal 
