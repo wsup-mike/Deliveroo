cf8e1362ec262695f955a81300854fdca3321de0
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-07 10:13:39 -0600

    Categories-in .map,edit { with ( for return statem

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -2,20 +2,22 @@ import { View, Text, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import CategoryCard from './CategoryCard';
 import { client } from '../sanity';
-import category from '../sanity/schemas/category';
+// import category from '../sanity/schemas/category';
 import { urlFor } from '../sanityUrl';
 
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
 
-  // useEffect(() => {
-  //   client.fetch(`
-  //     *[_type == 'category']
-  //   `).then(data => {
-  //     setCategories(data)
-  //   })
-  // }, []);
+  useEffect(() => {
+    client.fetch(`
+      *[_type == 'category']
+    `).then(data => {
+      setCategories(data)
+    })
+  }, []);
+
+  console.log(categories)
  
   return (
     <ScrollView
@@ -28,18 +30,18 @@ const Categories = () => {
     >
       {/* Category Cards here */}
     {/* <Text>Categories (Bitches!)</Text> */}
-    {/* {categories.map((category) => {
+    {categories.map((category) => (
       <CategoryCard 
         key={category._id}
         imgUrl={urlFor(category.image).width(200).url()}
         title={category.name} 
       />
-    })} */}
+    ))}
     
     {/* <CategoryCard imgUrl='https://links.papareact.com/gn7' title="Testing2" />  */}
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="TestingC" />
+    {/* <CategoryCard imgUrl='https://links.papareact.com/gn7' title="TestingC" />
     <CategoryCard imgUrl='https://links.papareact.com/gn7' title="TestingD" />
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="TestingE" />
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title="TestingE" /> */}
     </ScrollView>
   )
 }
