5ea4015bd32845f1539169ea95c6e5f5d28dac88
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 12:29:04 -0600

    Add <Image> now

--- components/DishRow.jsx
+++ components/DishRow.jsx
@@ -1,6 +1,7 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import Currency from 'react-currency-formatter'
+import { urlFor } from '../sanityUrl';
 
 const DishRow = ({
     id,
@@ -15,6 +16,12 @@ const DishRow = ({
             <Text className="text-lg mb-1">{name}</Text>
             <Text className="text-gray-400">{description}</Text>
             <Text className="text-gray-400 mt-2"><Currency quantity={price} currency="USD" /></Text>
+        </View>
+        <View>
+            <Image 
+                source={{ uri: urlFor(image).url() }}
+                className="h-20 w-20 bg-gray-300 p-4"
+            />
         </View>
     </TouchableOpacity>
   )
