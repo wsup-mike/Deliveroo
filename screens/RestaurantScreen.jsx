06bbaf0144d2af2e00c398032476e60f9fa42271
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 09:28:37 -0600

    import useNavigation fr reactnav/native

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -1,8 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/core';
+import { useNavigation } from '@react-navigation/native';
 
 const RestaurantScreen = () => {
+  
   // const route = useRoute();
   const { params: { // instead to destructure all this from RestaurantCard instead of using route.params.___ 
     id,
