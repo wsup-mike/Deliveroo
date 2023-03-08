ced17885b043bc3c82c7bff217e3542cc8729b44
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 11:21:49 -0600

    Map over 'dishes' array to return <DishRow>s here

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -79,6 +79,17 @@ const RestaurantScreen = () => {
           </TouchableOpacity>
           <View>
             <Text className="px-4 pt-4 mb-3 font-bold text-xl">Menu</Text>
+            {/* Dish rows here. To map over a restaurant's 'dishes[]' array to populate this area's content  */}
+            {dishes.map((dish) => (
+              <DishRow 
+                key={dish._id}
+                id={dish._id}
+                name={dish.name}
+                description={dish.description}
+                price={dish.price}
+                image={dish.image}
+              />
+            ))}
           </View>
       </View>
     </ScrollView>
