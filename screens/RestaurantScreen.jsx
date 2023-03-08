bab84a1794a13712d17237fbf60eaf4c7402a31e
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 10:22:03 -0600

    Add <StarIcon> and add 2 nested <Text> with green

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -3,7 +3,6 @@ import React, { useLayoutEffect } from 'react'
 import { useRoute } from '@react-navigation/core';
 import { useNavigation } from '@react-navigation/native';
 import { urlFor } from '../sanityUrl';
-import { ArrowLeftIcon } from 'react-native-heroicons/solid';
 import {
   ArrowLeftIcon,
   ChevronRightIcon,
@@ -56,7 +55,11 @@ const RestaurantScreen = () => {
           <View className="px-4 pt-4">
             <Text className="text-3xl font-bold">{title}</Text>
             <View className="flex-row space-x-2 my-1">
-
+              <View>
+                <StarIcon color="green" opacity={0.5} size={22} />
+                <Text className="text-green-500">{rating}<Text></Text>
+                </Text>
+              </View>
             </View>
           </View>
       </View>
