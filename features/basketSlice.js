222ac1406a048467dc9ee373254e22cacd83a0e3
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 16:14:08 -0600

    basketSlice.js-Cr8 and modify basketSlice template

---
+++ features/basketSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  items: [],
+}
+
+export const basketSlice = createSlice({
+  name: 'basket',
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {
+      state.items = [
+        ...state.items, // keep whatever's already there
+        action.payload, // tack on this payload's value to the items as well
+      ]
+    },
+    removeFromBasket: (state, action) => {
+      state.value -= 1
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = basketSlice.actions
+
+export default basketSlice.reducer
