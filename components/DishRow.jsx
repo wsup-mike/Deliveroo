2323cac53520b6fc7b37d5ac249eca449c333022
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 12:24:56 -0600

    Add currrency, price under description

--- components/DishRow.jsx
+++ components/DishRow.jsx
@@ -14,6 +14,7 @@ const DishRow = ({
         <View>
             <Text className="text-lg mb-1">{name}</Text>
             <Text className="text-gray-400">{description}</Text>
+            <Text className="text-gray-400 mt-2"><Currency quantity={price} currency="USD" /></Text>
         </View>
     </TouchableOpacity>
   )
