db32e786d7be1fbcf553f8989fbb715ab00f6003
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 10:55:50 -0600

    Grey 'Nearby {address}

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -62,10 +62,8 @@ const RestaurantScreen = () => {
                 </Text>
               </View>
               <View className="flex-row items-center space-x-1">
-                <StarIcon color="green" opacity={0.5} size={22} />
-                <Text className="text-xs text-gray-500">
-                  <Text className="text-green-500">{rating}</Text> · {genre}
-                </Text>
+                <MapPinIcon color="gray" opacity={0.4} size={22} />
+                <Text className="text-xs text-gray-500">Nearby · {address}</Text>
               </View>
             </View>
           </View>
