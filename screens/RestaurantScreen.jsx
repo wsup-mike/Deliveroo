d4e3d59b933f09df11cf9c880b26b9b5eef9fea0
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 10:52:39 -0600

    Duplicate star icon and text below it

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -61,6 +61,12 @@ const RestaurantScreen = () => {
                   <Text className="text-green-500">{rating}</Text> · {genre}
                 </Text>
               </View>
+              <View className="flex-row items-center space-x-1">
+                <StarIcon color="green" opacity={0.5} size={22} />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-500">{rating}</Text> · {genre}
+                </Text>
+              </View>
             </View>
           </View>
       </View>
