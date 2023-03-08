7e45b6f6cf05d08ee75358386c272052f4770512
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 09:10:38 -0600

    import n implement useRoute: route

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/core';
 
 const RestaurantScreen = () => {
+  const route = useRoute();
+
   return (
     <View>
       <Text>RestaurantScreen</Text>
