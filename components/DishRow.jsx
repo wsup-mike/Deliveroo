c740807340b5f6600a3c92d027a874c592fe6bd0
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 20:01:34 -0600

    Update conditional btn color at MinusCircleIcon

--- components/DishRow.jsx
+++ components/DishRow.jsx
@@ -61,7 +61,7 @@ const DishRow = ({
                             disabled={!items.length}
                         >
                             <MinusCircleIcon 
-                            color={items.length > 0 ? "00CCBB" : "gray"}
+                                color={items.length > 0 ? "#00CCBB" : "gray"}
                                 size={40}
                                 // color="#00ccbb"
                             />
