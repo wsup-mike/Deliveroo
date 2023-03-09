a0bdbb688b2502fa4de41910f8fe8ec84ef00fbd
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 20:47:41 -0600

    Style the 'View Basket' text

--- components/BasketIcon.jsx
+++ components/BasketIcon.jsx
@@ -18,9 +18,13 @@ const BasketIcon = () => {
             <TouchableOpacity
                 className="bg-[#00bbcc] mx-5 p-4 rounded-lg flex-row items-center space-x-1"
             >
-                <Text className="text-white font-extrabold text-lg bg-[#01a296] py-1 px-2">{items.length}</Text>
-                <Text>View Basket</Text>
-                <Text className="text-lg text-white font-extrabold"><Currency quantity={basketTotal} currency="USD" /></Text>
+                <Text className="text-white font-extrabold text-lg bg-[#01a296] py-1 px-2">
+                    {items.length}
+                </Text>
+                <Text className="flex-1 text-white font-extrabold text-lg text-center">View Basket</Text>
+                <Text className="text-lg text-white font-extrabold">
+                    <Currency quantity={basketTotal} currency="USD" />
+                </Text>
             </TouchableOpacity>
         </View>
     )
