f004d788089685e9fa44bbf110e8d7d19ed331cf
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 10:43:31 -0600

    run useSelector(selectRestarutn) n save to 'restau

--- screens/BasketScreen.jsx
+++ screens/BasketScreen.jsx
@@ -2,11 +2,12 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
+import { selectRestaurant } from '../features/restaurantSlice';
 
 // here we want to capture all details of the 'restaurant' 
 const BasketScreen = () => {
     const navigation = useNavigation();
-    
+    const restaurant = useSelector(selectRestaurant) // extracts restaurant name
     return (
     <View>
       <Text>BasketScreen</Text>
