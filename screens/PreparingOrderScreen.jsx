26c8454a601162afbe9d985519589dad57ed95e7
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 17:50:31 -0600

    PreparingOrderScrn-Add <Animatable.Image />

--- screens/PreparingOrderScreen.jsx
+++ screens/PreparingOrderScreen.jsx
@@ -1,31 +1,24 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import * as Animatable from 'react-native-animatable'
-import Video from 'react-native-video'
+// import Video from 'react-native-video'
+// import { Constants } from 'expo-av';
+// import { Platform } from 'react-native';
 
 
 const PreparingOrderScreen = () => {
   return (
     <SafeAreaView className="bg-[#00ccbb] flex-1 justify-center items-center">
-      {/* <Animatable.Image 
-        source={require('../assets/online-food-order-selection.mp4')}
+      <Animatable.Image 
+        source={require("../assets/motorbikes.gif")}
         animation="slideInUp"
         iterationCount={1}
-        className="h-96 w-96"
-        duration={4500}
-      /> */}
-
-      <Animatable.View animation="slideInUp">
-        <Video 
-            source={require('../assets/online-food-order-selection.mp4')}
-            style={{ width: 300, height: 300}}
-            muted={true}
-            repeat={true}
-            resizeMode="contain"
-        />
-      </Animatable.View>
+        className='h-60 w-60'
+      />
+      
+      
     </SafeAreaView>
   )
 }
 
-export default PreparingOrderScreen;
+export default PreparingOrderScreen;    
