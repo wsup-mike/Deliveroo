998056dfe4d2562c37053b862f257ca819a850dc
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 10:45:43 -0600

    useSelector(selectBasketItems) 2 get current items

--- screens/BasketScreen.jsx
+++ screens/BasketScreen.jsx
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
-import { selectRestaurant } from '../features/restaurantSlice';
+import { selectBasketItems, selectRestaurant } from '../features/restaurantSlice';
 
 // here we want to capture all details of the 'restaurant' 
 const BasketScreen = () => {
     const navigation = useNavigation();
     const restaurant = useSelector(selectRestaurant) // extracts restaurant name
+    const items = useSelector(selectBasketItems) // extracts all current items in basket
+
     return (
     <View>
       <Text>BasketScreen</Text>
