44a2d30a706a0307acff2c258a3a3f0fe4051ce6
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 20:05:27 -0600

    Cr8 'items' variable with useSelector(selectBasket

--- components/BasketIcon.jsx
+++ components/BasketIcon.jsx
@@ -1,14 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { selectBasketItems } from '../features/basketSlice';
 
 // Requires access to our basket!
 const BasketIcon = () => {
-  return (
-    <View>
-      <Text>BasketIcon</Text>
-    </View>
-  )
+    const items = useSelector(selectBasketItems);
+    return (
+        <View>
+        <Text>BasketIcon</Text>
+        </View>
+    )
 }
 
 export default BasketIcon
