12dcc8a4960951afaf687906e17d0427046af4ab
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 10:17:57 -0600

    Create new file in 'features': restaurantSlice.js

--- screens/BasketScreen.jsx
+++ screens/BasketScreen.jsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
-
+// here we want to capture all details of the 'restaurant' 
 const BasketScreen = () => {
     const navigation = useNavigation();
     return (
