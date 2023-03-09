8b8b052142a8b0d43c961c9a4bb03ee4587f4c47
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 14:57:09 -0600

    Add <Animatable.View<Video>

--- screens/PreparingOrderScreen.jsx
+++ screens/PreparingOrderScreen.jsx
@@ -1,11 +1,29 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import * as Animatable from 'react-native-animatable'
+import Video from 'react-native-video'
+
 
 const PreparingOrderScreen = () => {
   return (
     <SafeAreaView className="bg-[#00ccbb] flex-1 justify-center items-center">
-      <Text>PreparingOrderScreen</Text>
+      {/* <Animatable.Image 
+        source={require('../assets/online-food-order-selection.mp4')}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+        duration={4500}
+      /> */}
+
+      <Animatable.View animation="slideInUp">
+        <Video 
+            source={require('../assets/online-food-order-selection.mp4')}
+            style={{ width: 300, height: 300}}
+            muted={true}
+            repeat={true}
+            resizeMode="contain"
+        />
+      </Animatable.View>
     </SafeAreaView>
   )
 }
