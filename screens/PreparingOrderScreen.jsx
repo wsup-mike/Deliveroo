cd8a2a80584a856dc46ad9287ed2a770250a69d9
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 14:25:36 -0600

    Style SafeAreaView basic background

--- screens/PreparingOrderScreen.jsx
+++ screens/PreparingOrderScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const PreparingOrderScreen = () => {
   return (
-    <SafeAreaView >
+    <SafeAreaView className="bg-[#00ccbb] flex-1 justify-center items-center">
       <Text>PreparingOrderScreen</Text>
     </SafeAreaView>
   )
