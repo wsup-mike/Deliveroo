dbfa510521bdd7ff2b14116e461073ea39cd296b
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-08 20:06:51 -0600

    import useNavigation

--- components/BasketIcon.jsx
+++ components/BasketIcon.jsx
@@ -2,13 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { selectBasketItems } from '../features/basketSlice';
+import { useNavigation } from '@react-navigation/native';
+
 
 // Requires access to our basket!
 const BasketIcon = () => {
     const items = useSelector(selectBasketItems);
     return (
         <View>
-        <Text>BasketIcon</Text>
+            <Text>BasketIcon</Text> 
         </View>
     )
 }
