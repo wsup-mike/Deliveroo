30b368bcc0ece0099f9a236bd31f3f5aa5a0aa9f
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 14:24:06 -0600

    PreparingOrderScrn-import n wrap SafeAreaView

--- screens/PreparingOrderScreen.jsx
+++ screens/PreparingOrderScreen.jsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const PreparingOrderScreen = () => {
   return (
-    <View>
+    <SafeAreaView >
       <Text>PreparingOrderScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
