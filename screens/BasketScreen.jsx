26330f6674493229eaabe41427697200c6ee70c0
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 10:48:40 -0600

    import useDispatch

--- screens/BasketScreen.jsx
+++ screens/BasketScreen.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
 import { selectBasketItems, selectRestaurant } from '../features/restaurantSlice';
+import { useDispatch } from 'react-redux';
 
 // here we want to capture all details of the 'restaurant' 
 const BasketScreen = () => {
     const navigation = useNavigation();
     const restaurant = useSelector(selectRestaurant) // extracts restaurant name
     const items = useSelector(selectBasketItems) // extracts all current items in basket
+    
 
     return (
     <View>
