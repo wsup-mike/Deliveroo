cb6a2e85a1d346cc4bc5aa9671099c7b02abb485
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 19:39:48 -0600

    App.js-Add new screen: DeliveryScreen

--- App.js
+++ App.js
@@ -36,6 +36,14 @@ export default function App() {
               headerShown: false,
             }}
           />
+          <Stack.Screen
+            name="Delivery"
+            component={DeliveryScreen}
+            options={{
+              presentation: "fullScreenModal",
+              headerShown: false,
+            }}
+          />
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
