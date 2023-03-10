08e9a01360ae95e87fee1937dad2ad843ff0e83a
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 19:47:27 -0600

    import useSelector

--- screens/DeliveryScreen.jsx
+++ screens/DeliveryScreen.jsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import useNavigation from '@react-navigation/native'
+import { useSelector } from 'react-redux'
 
 const DeliveryScreen = () => {
-    const navigation = useNavigation();
+    const navigation = useNavigation(); // to include 'back' button
     return (
         <View>
         <Text>DeliveryScreen</Text>
