1ea544273645f651983b66317bde3282aa8e155f
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 19:48:26 -0600

    import useSelector to create 'restaurant' variable

--- screens/DeliveryScreen.jsx
+++ screens/DeliveryScreen.jsx
@@ -2,9 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import useNavigation from '@react-navigation/native'
 import { useSelector } from 'react-redux'
+import { selectRestaurant } from '../features/restaurantSlice';
 
 const DeliveryScreen = () => {
     const navigation = useNavigation(); // to include 'back' button
+    const restaurant = useSelector(selectRestaurant);
+    
     return (
         <View>
         <Text>DeliveryScreen</Text>
