2f160a860d3b0e48fe534d07b551eb09f51064e2
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 19:46:23 -0600

    DeliveryScreen-import useNavi n unlock navigation

--- screens/DeliveryScreen.jsx
+++ screens/DeliveryScreen.jsx
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import useNavigation from '@react-navigation/native'
 
 const DeliveryScreen = () => {
-  return (
-    <View>
-      <Text>DeliveryScreen</Text>
-    </View>
-  )
+    const navigation = useNavigation();
+    return (
+        <View>
+        <Text>DeliveryScreen</Text>
+        </View>
+    )
 }
 
 export default DeliveryScreen
