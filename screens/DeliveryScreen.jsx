36303f736df7e56f45e4bbdd06edfaa78793e1bc
Mike Ramos <michaelangeloram8@gmail.com>
2023-03-09 20:00:45 -0600

    Add onPress navigate to "Home"

--- screens/DeliveryScreen.jsx
+++ screens/DeliveryScreen.jsx
@@ -1,10 +1,10 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useSelector } from 'react-redux'
 import { selectRestaurant } from '../features/restaurantSlice';
 import { XMarkIcon } from 'react-native-heroicons/solid';
-import { TouchableOpacity } from 'react-native-web';
+
 
 const DeliveryScreen = () => {
     const navigation = useNavigation(); // to include 'back' button
@@ -14,7 +14,7 @@ const DeliveryScreen = () => {
         <View className="bg-[#00ccbb] flex-1">
             <SafeAreaView>
                 <View>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Home')}>
                         <XMarkIcon color="white" size={30} />
                     </TouchableOpacity>
                     <Text>Order Help</Text>
